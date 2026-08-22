f21aaad4cc86878929d1c1dda80d92971c078765
anima97833 <1285557437@qq.com>
2026-08-22 08:18:03 +0800

    feat(mansion): 信纸按钮精简为纯图标，支持自由/指定名士研墨写信模式

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 鸢小手机 Service Worker
-const CACHE_NAME = 'yuan-phone-pwa-v1787357551692';
+const CACHE_NAME = 'yuan-phone-pwa-v1787357877731';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
